1647b04f3017c182186355824a33d1befb547dd9
ricomercader <eduardoricomercader@gmail.com>
2024-11-04 15:47:34 -0500

    corrects typo on setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -27,7 +27,7 @@ echo "*** Installing Selenium and Chrome for BDD"
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sqlite3 ca-certificates chromium-driver python3-selenium
 
-echo "*** Installing Python depenencies..."
+echo "*** Installing Python dependencies..."
 source ~/venv/bin/activate && python3 -m pip install --upgrade pip wheel
 source ~/venv/bin/activate && pip install -r requirements.txt
 
